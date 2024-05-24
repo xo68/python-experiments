85a8fb2a0e3758d9e97e36c6eaeaf7dae565d445
Xavier Oswald <xoswald@gmail.com>
2024-05-24 22:58:57 +0200

    Fix some linting

--- sort.py
+++ sort.py
@@ -1,5 +1,5 @@
-# Return the index of the Smallest element
-def findSmallest(arr):
+# Find the smallest element
+def find_smallest(arr):
     smallest = arr[0]
     index = 0
     for i in range(1, len(arr)):
@@ -9,28 +9,28 @@ def findSmallest(arr):
     return index
 
 
-# Selection sort
-def selectionSort(arr):
-    newArr = []
-    for i in range(len(arr)):
-        index = findSmallest(arr)
-        newArr.append(arr.pop(index))
-    return newArr
+# Selection Sort
+def selection_sort(arr):
+    new_arr = []
+    for _ in range(len(arr)):
+        index = find_smallest(arr)
+        new_arr.append(arr.pop(index))
+    return new_arr
 
 
 # Quick sort
-def quickSort(arr):
+def quick_sort(arr):
     if len(arr) < 2:
         return arr
     else:
         pivot = arr[0]
         less = [i for i in arr[1:] if i <= pivot]
         greater = [i for i in arr[1:] if i > pivot]
-        return quickSort(less) + [pivot] + quickSort(greater)
+        return quick_sort(less) + [pivot] + quick_sort(greater)
 
 
 if __name__ == "__main__":
-    arr = [5, 3, 4, 1, 2, 0]
-    print(f"Array: {arr}")
-    print(f"Selection Sort: {selectionSort(arr[:])}")
-    print(f"Quick Sort: {quickSort(arr[:])}")
+    uarr = [5, 3, 4, 1, 2, 0]
+    print(f"Array: {uarr}")
+    print(f"Selection Sort: {selection_sort(uarr[:])}")
+    print(f"Quick Sort: {quick_sort(uarr[:])}")
